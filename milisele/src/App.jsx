52bfc740db1ce21956764c670297f972c014ef69
milisanchez <104220383+milisanchez@users.noreply.github.com>
2023-10-05 10:49:20 -0300

    Add files via upload
    Santi estuvo por acá

---
+++ milisele/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      export default function Bio() 
+      return (
+     <div class="intro">
+      <h1>Welcome to my website!</h1>
+      </div>
+    <p class="summary">
+    <br></br>
+    You can find my thoughts here.
+    <b>And<i>pictures</i> of scientists!</b>
+    </p>
+  );
+  
+    </>
+  )
+}
+
+export default App
